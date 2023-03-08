dc21363a56c06902f848e6f7426bbbae92d370d8
meet-javiya0 <meetjaviya29@gmail.com>
2023-03-08 10:26:13 +0530

    Uploaded via GitHub Desktop

---
+++ Database/app/src/main/java/com/example/database/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.database
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity : AppCompatActivity() {
+    lateinit var database : DatabaseReference
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+        val etName = findViewById<TextInputEditText>(R.id.etName)
+        val etEmail = findViewById<TextInputEditText>(R.id.etEmail)
+        val etPassword = findViewById<TextInputEditText>(R.id.etPassword)
+        val etUserName = findViewById<TextInputEditText>(R.id.etUserName)
+
+        btnRegister.setOnClickListener {
+            val name = etName.text.toString()
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+            val username = etUserName.text.toString()
+
+            val user = Users(name,email,password,username)
+            database = FirebaseDatabase.getInstance().getReference("Users")
+            database.child(username).setValue(user).addOnSuccessListener {
+                Toast.makeText(this,"User Registered Successfully",Toast.LENGTH_SHORT).show()
+                etName.setText("")
+                etEmail.setText("")
+                etPassword.setText("")
+                etUserName.setText("")
+            }.addOnFailureListener{
+                Toast.makeText(this,"Failed to register",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
---
+++ Database/app/src/main/java/com/example/database/Users.kt
@@ -0,0 +1,5 @@
+package com.example.database
+
+data class Users(val name : String,val email : String,val password : String,val userId : String){
+
+}
