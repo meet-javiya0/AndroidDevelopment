eefd737a5b7a87703b837b908df7273e61b647d9
meet-javiya0 <meetjaviya29@gmail.com>
2023-03-16 18:18:53 +0530

    Uploaded via GitHub Desktop

--- Database/app/src/main/java/com/example/database/MainActivity.kt
+++ Database/app/src/main/java/com/example/database/MainActivity.kt
@@ -1,17 +1,15 @@
 package com.example.database
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
-    lateinit var database : DatabaseReference
+    private lateinit var database : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
