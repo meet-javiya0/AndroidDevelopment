7cf075baccacb534f3f302e99f9727dbdac43d61
meet-javiya0 <meetjaviya29@gmail.com>
2023-04-19 23:21:05 +0530

    Uploaded via GitHub Desktop

--- API/app/src/main/java/com/example/api/MainActivity.kt
+++ API/app/src/main/java/com/example/api/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -11,10 +13,13 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    lateinit var recyclerView: RecyclerView
+    lateinit var myAdapter: MyAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         val retrofitBuilder = Retrofit.Builder()
             .baseUrl("https://dummyjson.com/")
             .addConverterFactory(GsonConverterFactory.create())
@@ -28,15 +33,9 @@ class MainActivity : AppCompatActivity() {
                 // if api call is success, the use the data in your app and show it on your app
                 val responseBody = response.body()
                 val productList = responseBody?.products!!
-
-                val collectDataInSB = StringBuilder()
-
-                for (myData in productList){
-                    collectDataInSB.append(myData.title + " ")
-                }
-
-                val tV = findViewById<TextView>(R.id.textView)
-                tV.text = collectDataInSB
+                myAdapter = MyAdapter(this@MainActivity, productList)
+                recyclerView.adapter = myAdapter
+                recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
             }
 
             override fun onFailure(call: Call<MyData?>, t: Throwable) {
--- API/app/src/main/java/com/example/api/MyAdapter.kt
+++ API/app/src/main/java/com/example/api/MyAdapter.kt
@@ -1,18 +1,20 @@
 package com.example.api
 
 import android.app.Activity
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.imageview.ShapeableImageView
+import com.squareup.picasso.Picasso
 
-class MyAdapter(val context : Activity, private val productArrayList : List<Product>)
+class MyAdapter(private val context : Activity, private val productArrayList : List<Product>)
     : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val title : TextView
-        private val image : ShapeableImageView
+        val title : TextView
+        val image : ShapeableImageView
 
         init {
             title = itemView.findViewById(R.id.productTitle)
@@ -21,11 +23,16 @@ class MyAdapter(val context : Activity, private val productArrayList : List<Prod
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-
+        val itemView = LayoutInflater.from(context).inflate(R.layout.each_item, parent,false)
+        return MyViewHolder(itemView)
     }
 
-    override fun onBindViewHolder(holder: MyAdapter.MyViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentItem = productArrayList[position]
+        holder.title.text = currentItem.title
 
+        // load image from the url using picasso
+        Picasso.get().load(currentItem.thumbnail).into(holder.image)
     }
 
     override fun getItemCount(): Int {
