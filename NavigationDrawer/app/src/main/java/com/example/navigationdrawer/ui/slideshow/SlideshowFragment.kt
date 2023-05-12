858b3a9c9acc6c6b0a0295596f334970daad2e38
meet-javiya0 <meetjaviya29@gmail.com>
2023-05-12 16:18:54 +0530

    Completed the navigation view concept

---
+++ NavigationDrawer/app/src/main/java/com/example/navigationdrawer/ui/slideshow/SlideshowFragment.kt
@@ -0,0 +1,31 @@
+package com.example.navigationdrawer.ui.slideshow
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.navigationdrawer.databinding.FragmentSlideshowBinding
+
+class SlideshowFragment : Fragment() {
+
+    private var _binding: FragmentSlideshowBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
